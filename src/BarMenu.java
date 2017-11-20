a1b06fca843e264740deddd703f210362e2a15cf
marcus_s_lepp@hotmail.com <marcus_s_lepp@hotmail.com>
2017-11-20 09:05:22 +0100

    files added not completed

---
+++ src/BarMenu.java
@@ -0,0 +1,10 @@
+
+public class BarMenu extends FlightMenu{
+
+	
+	BarMenu barMenu1 = new BarMenu();
+	BarMenu barMenu2 = new BarMenu();
+	BarMenu barMenu3 = new BarMenu();
+	BarMenu barMenu4 = new BarMenu();
+	BarMenu barMenu5 = new BarMenu();
+}
---
+++ src/FirstClassMenu.java
@@ -0,0 +1,11 @@
+
+public class FirstClassMenu extends FlightMenu{
+
+	
+	
+	
+	
+	
+	
+	
+}
