55720339aee2b62f17d88be1b3a6931503baa0f7
Tommy Steger <tommysteger@gmail.com>
2017-11-21 16:13:08 +0100

    JUnit test beta of booking class

--- test/booking/BookingTest.java
+++ test/booking/BookingTest.java
@@ -6,14 +6,71 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BookingTest {
+	private Booking booking = new Booking();
 
 	@BeforeEach
 	void setUp() throws Exception {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testBookOnSeatOnSecondClass() {
+	
+		
+		String sRetInfo=booking.bookFlightAndSeats(0,0,1);
+		assertEquals("Boking Flight", sRetInfo.substring(0,13));
+	}
+	@Test
+	void testBookFiveSeatOnSecondClass() {
+	
+		
+		String sRetInfo=booking.bookFlightAndSeats(0,0,5);
+		assertEquals("Boking Flight", sRetInfo.substring(0,13));
+	}
+	@Test
+	void testBookSixSeatOnSecondClass() {
+	
+		
+		String sRetInfo=booking.bookFlightAndSeats(0,0,6);
+		assertEquals("There are only", sRetInfo.substring(0,14));
+	}
+	@Test
+	void testBookSixSeparateSeatOnSecondClass() {
+	
+		
+		String sRetInfo=booking.bookFlightAndSeats(0,0,2);
+		sRetInfo=booking.bookFlightAndSeats(0,0,2);
+		sRetInfo=booking.bookFlightAndSeats(0,0,2);
+		assertEquals("There are only", sRetInfo.substring(0,14));
+	}
+	@Test
+	void testBookOnSeatOnFirstClass() {
+	
+		
+		String sRetInfo=booking.bookFlightAndSeats(0,1,1);
+		assertEquals("Boking Flight", sRetInfo.substring(0,13));
+	}
+	@Test
+	void testBookFiveSeatOnFirstClass() {
+	
+		
+		String sRetInfo=booking.bookFlightAndSeats(0,1,5);
+		assertEquals("Boking Flight", sRetInfo.substring(0,13));
+	}
+	@Test
+	void testBookSixSeatOnFirstClass() {
+	
+		
+		String sRetInfo=booking.bookFlightAndSeats(0,1,6);
+		assertEquals("There are only", sRetInfo.substring(0,14));
+	}
+	@Test
+	void testBookSixSeparateSeatOnFirstClass() {
+	
+		
+		String sRetInfo=booking.bookFlightAndSeats(0,1,2);
+		sRetInfo=booking.bookFlightAndSeats(0,1,2);
+		sRetInfo=booking.bookFlightAndSeats(0,1,2);
+		assertEquals("There are only", sRetInfo.substring(0,14));
 	}
 
 }
